5a10c06f6a1c824554c6eba0b28002784647e09d
vathunt <67134348+vathunt@users.noreply.github.com>
2021-04-12 09:01:50 +0700

    Update script.js

--- res/script.js
+++ res/script.js
@@ -27,7 +27,7 @@ if (localStorage.getItem("skorPlayer")) {
 }
 
 startGame.addEventListener("click", () => {
-	splashScreen.style.top = "-10vh";
+	splashScreen.style.top = "-120vh";
 	splashScreen.style.transition = ".75s";
 });
 
